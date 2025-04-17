5b77ce080c8cb3743961bdf84bed5acfbd52c4a0
Vincenzo Scognamiglio <vincenzo.scognamiglio2@unina.it>
2025-04-17 10:18:10 +0200

    added headless simulation for ldc

--- launch/launch_leo_field_headless.py
+++ launch/launch_leo_field_headless.py
@@ -1,7 +1,7 @@
 import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.actions import ExecuteProcess
+from launch.actions import ExecuteProcess, SetEnvironmentVariable
 from launch.actions import OpaqueFunction
 from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
